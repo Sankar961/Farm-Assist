e30b8d93382fc3dd0852356e911c40c2410b1160
Sankar <sankar961a@gmail.com>
2020-10-23 19:47:50 +0530

    login_table

--- login_details.sql
+++ login_details.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2020 at 04:10 PM
+-- Generation Time: Oct 23, 2020 at 04:16 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -35,20 +35,6 @@ CREATE TABLE `login_details` (
   `register_at` datetime DEFAULT NULL,
   `last_login` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `login_details`
---
-
-INSERT INTO `login_details` (`id`, `emailid`, `password`, `account_type`, `register_at`, `last_login`) VALUES
-(4, 'sankar961a@gmail.com', 'bbae5703fa28dfb6515870388c81de45', '1', '2020-10-16 18:31:30', NULL),
-(5, 'sankar961a@gmail.com', 'bbae5703fa28dfb6515870388c81de45', '1', '2020-10-16 19:09:56', NULL),
-(6, 'sankar961a@gmail.com', 'bbae5703fa28dfb6515870388c81de45', '2', '2020-10-17 20:08:09', NULL),
-(7, 'sankar961a@gmail.com', 'bbae5703fa28dfb6515870388c81de45', '1', '2020-10-19 18:23:34', NULL),
-(8, 'sankar961a@gmail.com', '57c046325ad38199b15078548967b1df', '2', '2020-10-21 11:45:58', NULL),
-(9, 'sankardas@gmail.com', '67164b7de9d39294681ba0990dc719e9', '2', '2020-10-21 11:47:24', NULL),
-(10, 'sankardas@gmail.com', '67164b7de9d39294681ba0990dc719e9', '2', '2020-10-21 11:49:53', NULL),
-(11, 'sankardas@gmail.com', 'bbae5703fa28dfb6515870388c81de45', '2', '2020-10-21 11:57:16', NULL);
 
 --
 -- Indexes for dumped tables
